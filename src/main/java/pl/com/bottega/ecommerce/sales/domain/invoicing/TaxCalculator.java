1d4db5deff29f5513abf3ea11a1c8b83d01253b5
Jakub Stawiszyński <kubastyk@wp.pl>
2017-03-28 14:03:22 +0200

    Create TaxCalculator class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
@@ -0,0 +1,26 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class TaxCalculator {
+
+	public Tax calculateTax(ProductType type, Money price) {
+		TaxCreator creator;
+		switch (type) {
+		case DRUG:
+			creator = new DrugTax();
+			break;
+		case FOOD:
+			creator = new FoodTax();
+			break;
+		case STANDARD:
+			creator = new StandardTax();
+			break;
+
+		default:
+			throw new IllegalArgumentException();
+		}
+		return creator.createTax(price);
+	}
+}
